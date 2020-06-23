cc77b5ccc3437801e492cb7c609c825caa2460d5
rafael <rafaelllparaynooo23@gmail.com>
2020-06-23 17:30:14 +0800

    git init

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const posts = [
+    {
+        username: 'kyle',
+        title: 'Post 1',
+    },
+    {
+        username: 'Jimm',
+        title: 'Post 2',
+    }
+
+]
+
+app.get('/posts', (req, res) => {
+    res.json(posts);
+})
+
+app.listen(3000);
