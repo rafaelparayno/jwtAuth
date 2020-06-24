bbf1572ad4a1f5859499a65bbef8aea76d169439
rafael <rafaelllparaynooo23@gmail.com>
2020-06-24 16:07:38 +0800

    fixing

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
+
+const jwt = require('jsonwebtoken');
+
+app.use(express.json())
 
 const posts = [
     {
@@ -19,6 +25,11 @@ app.get('/posts', (req, res) => {
 
 app.get('/login', (req, res) => {
 
+    const username = req.body.username;
+    const user = { name: username }
+
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({ accessToken: accessToken })
 })
 
 app.listen(3000);
