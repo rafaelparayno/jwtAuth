56a854b3507a052f55808119b12e61c655a4b7e0
rafael <rafaelllparaynooo23@gmail.com>
2020-06-25 00:18:21 +0800

    fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 const jwt = require('jsonwebtoken');
 
-app.use(express.json())
+app.use(express.json());
 
 const posts = [
     {
@@ -19,17 +19,31 @@ const posts = [
 
 ]
 
-app.get('/posts', (req, res) => {
-    res.json(posts);
+app.get('/posts', authenticateToken, (req, res) => {
+
+    res.json(posts.filter(post => post.username === req.user.name));
 })
 
-app.get('/login', (req, res) => {
+app.post('/login', (req, res) => {
 
-    const username = req.body.username;
+    const username = req.body.username
     const user = { name: username }
 
     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
     res.json({ accessToken: accessToken })
 })
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.send(403)
+        req.user = user
+        next()
+    })
+}
 
 app.listen(3000);
