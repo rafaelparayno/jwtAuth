177d068494ca749e7054f1cfe0f24a9ab7dcab7b
rafael <rafaelllparaynooo23@gmail.com>
2020-06-25 16:30:25 +0800

    done with jwt

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const posts = [
         title: 'Post 1',
     },
     {
-        username: 'Jimm',
+        username: 'rafael',
         title: 'Post 2',
     }
 
@@ -24,14 +24,6 @@ app.get('/posts', authenticateToken, (req, res) => {
     res.json(posts.filter(post => post.username === req.user.name));
 })
 
-app.post('/login', (req, res) => {
-
-    const username = req.body.username
-    const user = { name: username }
-
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    res.json({ accessToken: accessToken })
-})
 
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization']
@@ -40,7 +32,7 @@ function authenticateToken(req, res, next) {
     if (token == null) return res.sendStatus(401)
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.send(403)
+        if (err) return res.sendStatus(403)
         req.user = user
         next()
     })
